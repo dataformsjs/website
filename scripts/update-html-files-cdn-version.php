1561b492b6e54ca02b2aacc1f2ee2dcb71ed7295
Conrad Sollitt <57777521+ConradSollitt@users.noreply.github.com>
2020-11-19 01:00:27 -0800

    🚀 Updated HTML files for new npm build

--- scripts/update-html-files-cdn-version.php
+++ scripts/update-html-files-cdn-version.php
@@ -15,8 +15,8 @@
 // instead to sync all changes from GitHub.
 
 const CDN = 'https://cdn.jsdelivr.net/npm/dataformsjs@';
-const PREV_VER = '4.7.1';
-const NEW_VER = '4.8.0';
+const PREV_VER = '4.8.0';
+const NEW_VER = '5.0.0';
 const SEARCH_TEXT = CDN . PREV_VER;
 const REPLACE_TEXT = CDN . NEW_VER;
 
